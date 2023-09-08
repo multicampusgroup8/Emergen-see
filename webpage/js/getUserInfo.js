4c25cd29fd61165d7c741e66a2892968efb3231e
jyc0011 <jyc0011@kau.kr>
2023-09-08 13:04:27 +0900

    js 수정완료

--- webpage/js/getUserInfo.js
+++ webpage/js/getUserInfo.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-    fetch('/path/to/user/data')  // 백엔드 API 엔드포인트
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/path/to/user/data')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -7,12 +7,31 @@ window.onload = function() {
             return response.json();
         })
         .then(data => {
-            // 사용자 이름 및 프로필 이미지를 페이지에 채웁니다.
+            // 사용자 정보를 페이지에 채웁니다.
             document.getElementById('userName').textContent = data.name;
             document.getElementById('profileImg').src = data.profileImage;
+
+            // 로그인 상태이므로 로그아웃 버튼을 보여줍니다.
+            document.getElementById('logoutButton').style.display = "inline-block";
+
+            // 로그인 및 회원가입 버튼 숨기기
+            document.getElementById('loginButton').style.display = "none";
+            document.getElementById('signupButton').style.display = "none";
         })
         .catch(error => {
             console.error('There was a problem fetching user data:', error);
-            document.getElementById('userName').textContent = 'Error loading user name';
+            document.getElementById('userName').textContent = '로그인이 필요합니다.';
+            
+            // 유저 프로필 이미지와 유저 이름 숨기기
+            document.getElementById('userName').style.display = "none";
+            document.getElementById('profileImg').style.display = "none";
+        
+            // 로그인 및 회원가입 버튼 보여주기
+            document.getElementById('loginButton').style.display = "inline-block";
+            document.getElementById('signupButton').style.display = "inline-block";
+        
+            // 로그아웃 버튼 숨기기
+            document.getElementById('logoutButton').style.display = "none";
         });
-}
+        
+});
