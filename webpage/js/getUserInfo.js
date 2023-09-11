335a2388dfaefb4dc2e453a68d0273997d7ee56a
jyc0011 <jyc0011@kau.kr>
2023-09-11 10:32:47 +0900

    로그인/회원가입&로그아웃 뜨는거 수정

--- webpage/js/getUserInfo.js
+++ webpage/js/getUserInfo.js
@@ -10,28 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
             // 사용자 정보를 페이지에 채웁니다.
             document.getElementById('userName').textContent = data.name;
             document.getElementById('profileImg').src = data.profileImage;
-
-            // 로그인 상태이므로 로그아웃 버튼을 보여줍니다.
-            document.getElementById('logoutButton').style.display = "inline-block";
-
-            // 로그인 및 회원가입 버튼 숨기기
-            document.getElementById('loginButton').style.display = "none";
-            document.getElementById('signupButton').style.display = "none";
         })
         .catch(error => {
             console.error('There was a problem fetching user data:', error);
-            document.getElementById('userName').textContent = '로그인이 필요합니다.';
-            
-            // 유저 프로필 이미지와 유저 이름 숨기기
-            document.getElementById('userName').style.display = "none";
-            document.getElementById('profileImg').style.display = "none";
-        
-            // 로그인 및 회원가입 버튼 보여주기
-            document.getElementById('loginButton').style.display = "inline-block";
-            document.getElementById('signupButton').style.display = "inline-block";
-        
-            // 로그아웃 버튼 숨기기
-            document.getElementById('logoutButton').style.display = "none";
+            document.getElementById('userName').textContent = 'Network error';
+
         });
         
 });
