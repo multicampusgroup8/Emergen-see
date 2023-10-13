9da107ee7bb439ebd67718e92b9cccea894aeec4
2pearl98 <45809423+2pearl98@users.noreply.github.com>
2023-10-13 11:29:32 +0900

    home, user 컨트롤러

---
+++ backend/emergenseeSpring/src/main/java/com/mulcam8/emergensee/controller/UserController.java
@@ -0,0 +1,101 @@
+package com.multicampus.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.multicampus.web.service.UserServiceImpl;
+import com.multicampus.web.vo.UserVO;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+
+	@GetMapping("/register")
+	public String register() {
+		
+		return "users/register";
+	}
+	@GetMapping("/findID")
+	public String findID() {
+		
+		return "users/findID";
+	}
+	@GetMapping("/findPassword")
+	public String findPassword() {
+		
+		return "users/findPassword";
+	}
+	
+	
+	@RequestMapping(value = "/login", method=RequestMethod.GET)
+	public String login() {
+		
+		return "users/login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginCheck(@RequestParam("userid") String userid, @RequestParam("password") String password
+			, HttpServletResponse response
+			, HttpServletRequest request ) throws Exception {
+			
+		String path = "";
+		
+		//로그인 정보 확인
+		UserVO vo = new UserVO();
+		
+		vo.setUserid(userid);
+		vo.setUserpwd(password);
+		
+		int result = userServiceImpl.loginCheck(vo);
+		
+		if(result == 1) {
+//			if(keeplogin != null) {
+//				//쿠키 등록
+//				Cookie cookie = new Cookie("username",userid);
+//				cookie.setMaxAge(60*60*24*7);
+//				cookie.setPath("/");
+//				
+//				response.addCookie(cookie);
+//			}
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("logId", userid);
+			session.setAttribute("logStatus", "Y");
+			session.setMaxInactiveInterval(1200);
+			
+			
+			path = "redirect:/";
+		}else {
+			path = "users/login";
+		}
+		
+		return path;	
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		return "users/logout";
+	}
+	
+	@RequestMapping("/logout")
+	public String logoutCheck(@RequestParam("yon") String yon, HttpSession session) {	
+		System.out.println("yon의 값: "+yon);
+		if(yon.equals("예(Yes)")) {
+			System.out.println("세션 지우기");
+			session.invalidate();
+		}else if(yon.equals("아니요(No)")) {
+			System.out.println("세션 안지우기");
+		}
+		return "redirect:/";
+	}
+}
