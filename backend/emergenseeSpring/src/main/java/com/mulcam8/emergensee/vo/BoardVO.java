df9bd158b3a5aae9b84f5255ffaec9c799eb26fe
musso91hyukjun <141107853+musso91hyukjun@users.noreply.github.com>
2023-10-17 09:57:50 +0900

    Add files via upload

---
+++ backend/emergenseeSpring/src/main/java/com/mulcam8/emergensee/vo/BoardVO.java
@@ -0,0 +1,95 @@
+package com.mulcam8.emergensee.vo;
+
+
+public class BoardVO {
+	private int post_no;
+	private String subject;
+	private String contents;
+	private String userid;
+	private String writedate;
+	private String datetime;
+	private String hit;
+	private String notice;
+	private String popular;
+	private String disasterType;
+	private String location;
+	private String user_user_no;
+	public int getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(int post_no) {
+		this.post_no = post_no;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContent(String content) {
+		this.contents = content;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getHit() {
+		return hit;
+	}
+	public void setHit(String hit) {
+		this.hit = hit;
+	}
+	public String getNotice() {
+		return notice;
+	}
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+	public String getPopular() {
+		return popular;
+	}
+	public void setPopular(String popular) {
+		this.popular = popular;
+	}
+	public String getDisasterType() {
+		return disasterType;
+	}
+	public void setDisasterType(String disasterType) {
+		this.disasterType = disasterType;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getUser_user_no() {
+		return user_user_no;
+	}
+	public void setUser_user_no(String user_user_no) {
+		this.user_user_no = user_user_no;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	
+	
+}
---
+++ backend/emergenseeSpring/src/main/java/com/mulcam8/emergensee/vo/PagingVO.java
@@ -0,0 +1,122 @@
+package com.mulcam8.emergensee.vo;
+
+
+public class PagingVO {
+	private int nowPage = 1; 			// 현재페이지
+	private int onePageRecord = 5; 		// 한번에 표시할 레코드 수
+	
+	private int totalRecord; 			// 총 레코드 수
+	private int totalPage; 				// 총 페이지 수
+	
+	private int offsetPoint = (nowPage-1)*onePageRecord; // 레코드 선택시 시작위치
+	
+	// 페이지 넘버링
+	private int onePageCount = 5;
+	private int startPage = 1;
+	
+	private String searchKey;
+	private String searchText;
+	
+	private String disasterType;
+	
+	
+	public int getNowPage() {
+		return nowPage;
+	}
+
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+		
+		offsetPoint = (nowPage-1)*onePageRecord;
+		
+		startPage = (nowPage - 1)/onePageCount*onePageCount+1;
+	}
+
+	public int getOnePageRecord() {
+		return onePageRecord;
+	}
+
+	public void setOnePageRecord(int onePageRecord) {
+		this.onePageRecord = onePageRecord;
+	}
+
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+		
+		// 총 페이지 수
+		totalPage = (int)Math.ceil(totalRecord / (double)onePageRecord);
+	}
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getOffsetPoint() {
+		return offsetPoint;
+	}
+
+	public void setOffsetPoint(int offsetPoint) {
+		this.offsetPoint = offsetPoint;
+	}
+
+	public int getOnePageCount() {
+		return onePageCount;
+	}
+
+	public void setOnePageCount(int onePageCount) {
+		this.onePageCount = onePageCount;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public String getSearchKey() {
+		return searchKey;
+	}
+
+	public void setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
+	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+
+	public String getDisasterType() {
+		return disasterType;
+	}
+
+	public void setDisasterType(String disasterType) {
+		this.disasterType = disasterType;
+	}
+
+	@Override
+	public String toString() {
+		return "PagingVO [nowPage=" + nowPage + ", onePageRecord=" + onePageRecord + ", totalRecord=" + totalRecord
+				+ ", totalPage=" + totalPage + ", offsetPoint=" + offsetPoint + ", onePageCount=" + onePageCount
+				+ ", startPage=" + startPage + ", searchKey=" + searchKey + ", searchText=" + searchText
+				+ ", disasterType=" + disasterType + "]";
+	}
+
+	
+	
+	
+	
+}
